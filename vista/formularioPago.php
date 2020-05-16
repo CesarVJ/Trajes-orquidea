b9bf38fdef8994022ea19f988e4a43689a101ecb
cesar valdez jacinto <cvj991@gmail.com>
2020-05-15 22:29:19 -0500

    Cambios en el formulario de pago

--- vista/formularioPago.php
+++ vista/formularioPago.php
@@ -29,40 +29,42 @@ if(!empty(trim($_POST["cantidad-articulos"])) & !empty(trim($_POST['precioFinal'
 	<div class="contenido-body">
 		<?php require_once("../componentes/menu.php") ?>
 		<h1 class="titulo-Catalogo">Trajes Orquidea</h1>
-		<div class="contenedor-general container">
+		<form class="contenedor-general container" method="post" action="pagoExitoso.php?id=<?php echo $articulo."&cantidad=".$cantidad."&total=".$total_pagar;?>">
 			<div class="contenedor-pago">
 				<h1 id="titulo">Tarjeta de Debito/Credito</h1>
 
 				<div class="nombre-titular">
-					<p>Nombre del titular:</p>
+					<p class="titulo-pago">Nombre del titular:</p>
 					<input type="text" name="nombre-titular" class="texto">
 				</div>
 
 				<div class="numero-tarjeta">
-					<p>Número de la tarjeta:</p>
+					<p class="titulo-pago">Número de la tarjeta:</p>
 					<input type="text" name="numero-tarjeta" class="texto">
 				</div>
 
 				<div class="fecha-expiracion">
-					<p>Fecha de expiración:</p>
+					<p class="titulo-pago">Fecha de expiración:</p>
 					<input type="month" name="fecha-expiracion" class="texto">
 				</div>
 
 				<div class="codigo-seguridad">
-					<p>Código de seguridad:</p>
+					<p class="titulo-pago">Código de seguridad:</p>
 					<input id="codigo-seguridad" type="number" max="999" name="codigo-seguridad" class="texto">
 				</div>
 			</div>
 
 			<div class="mostrar-pago">
 				<h1>Total a pagar: <?php echo $total_pagar;?><span id="total-pagar">$</span></h1>
-				<h1>Usted ha seleccionado: <span id="cantidad"></span></h1>
-				<h3><?php echo $producto->getNombre_producto();?> X <?php echo $cantidad;?> </h3>
+				<h2>Usted ha seleccionado: <span id="cantidad"></span></h2>
+				<ul>
+				<li><h3><?php echo $producto->getNombre_producto();?> X <?php echo $cantidad;?> </h3></li>
+				</ul>
 
 
 				<a href="#" onclick="window.history.go(-1); return false;"><button id="btn-regresar" class="btn btn-outline-danger btn-lg btn-block">Regresar</button></a>
 				<input type="submit" id="btn-comprar" class="btn btn-success btn-lg btn-block" value="Comprar Articulo(s)">
-			</div>
+</form>
 		</div>
 
 </body>
