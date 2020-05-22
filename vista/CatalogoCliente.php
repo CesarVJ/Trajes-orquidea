4de096ae952ab35fb01e0f8b20fed8d47ffb1655
cesar valdez jacinto <cvj991@gmail.com>
2020-05-22 11:04:43 -0500

    Se creo la ventana de confirmacion al cancelar un pedido

--- vista/CatalogoCliente.php
+++ vista/CatalogoCliente.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../css/estilos.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../css/buscar.css?v=<?php echo time(); ?>">
 
-
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <title>Trajes Orquidea</title>
 </head>
 
@@ -42,10 +42,16 @@
         <h1 class="titulo-Catalogo">Trajes Orquidea</h1>
         <div class="container">
         <form id="form-buscar" action="">
-            <input id="barra-busqueda" type="search" name="" id=""  placeholder="Buscar articulo">
+            <input id="barra-busqueda" type="text" name="" id=""  placeholder="Buscar articulo">
             <input id="btn-buscar" class="btn btn-outline-primary" type="submit" value="Buscar">
         </form>
             <div class="contenedor row">
+        <script>
+            $('#barra-busqueda').on("keyup input", function(){
+                console.log("Presionaste");
+            });
+        </script>
+            
                 <?php
                         require('../modelo/Producto.php');
 
@@ -81,9 +87,6 @@
         <?php require_once("../componentes/aside.html");?>
     </div>
     <?php require_once("../componentes/footer.html");?>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
--- vista/misCompras.php
+++ vista/misCompras.php
@@ -32,20 +32,20 @@
         <?php
                 if(isset($_GET['operacion'])){
                     if(trim($_GET['operacion']) == 'cancelar'){?>
-	<div class="alert alert-success alert-dismissible fade show" role="alert">
-		<strong>Su compra se ha cancelado con exito</strong>
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-	</div>
-	<?php }else {?>
-	<div class="alert alert-danger alert-dismissible fade show" role="alert">
-		<strong>!Algo ha salido mal!.</strong>
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-	</div>
-	<?php
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Su compra se ha cancelado con exito</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <?php }else {?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>!Algo ha salido mal!.</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <?php
 
         }
         }
@@ -67,37 +67,62 @@
                         <p><b>Llega: </b><?php echo $listaCompras->getFecha_llegada(); ?></p>
                         <?php
                             if($listaCompras->getEstado_compra() == "cancelado"){ ?>
-                                <a href="#" class="btn btn-secondary deshabilitado">Pedido cancelado</a>                            
-                            <?php }else{
+                        <a href="#" class="btn btn-secondary deshabilitado">Pedido cancelado</a>
+                        <?php }else{
                             if(strtotime($date) < strtotime($listaCompras->getFecha_llegada())){ ?>
-                                <a href="../cancelarPedido.php?id=<?php echo $listaCompras->getId_compra(); ?>" class="btn btn-danger">Cancelar pedido</a>                            
+                        <button class="btn btn-danger" data-toggle="modal"
+                            data-target="#mensaje-cancelar<?php echo $listaCompras->getId_compra();?>">Cancelar
+                            pedido</button>
                         <?php }else{ ?>
-                            <a href="#" class="btn btn-secondary deshabilitado">Pedido enviado</a>                            
-                        <?php   } }
-                        ?>                        
+                        <a href="#" class="btn btn-secondary deshabilitado">Pedido enviado</a>
+                        <?php   } } ?>
+                    </div>
+                </div>
+                <div class="modal fade" id="mensaje-cancelar<?php echo $listaCompras->getId_compra();?>" tabindex="-1"
+                    role="dialog" aria-labelledby="mensaje-cancelar" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="mensaje-cancelar">Cancelar pedido</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ¿Esta seguro de que desea cancelar su pedido?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Descartar</button>
+                                <a class="btn-cancelar"
+                                        href="../cancelarPedido.php?id=<?php echo $listaCompras->getId_compra();?>" style="color:#fff;text-decoration:none;"><button type="button" class="btn btn-danger">Cancelar pedido</button></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <?php } ?>
-                <script>
-                $('.btn.deshabilitado').css("pointerEvents","none");
-                $('.btn.deshabilitado').css("cursor","default");
-                </script>
+                    <?php } ?>
+                    <script>
+                        $('.btn.deshabilitado').css("pointerEvents", "none");
+                        $('.btn.deshabilitado').css("cursor", "default");
+                    </script>
+                </div>
             </div>
         </div>
-    </div>
-    <?php include_once("../componentes/footer.html") ?>
-    <script src="../js/validaciones.js?v=<?php echo time(); ?>"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
- <script src="https://account.snatchbot.me/script.js"></script><script>window.sntchChat.Init(109433)</script> 
+        <?php include_once("../componentes/footer.html") ?>
+        <script src="../js/validaciones.js?v=<?php echo time(); ?>"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+        </script>
+        <script src="https://account.snatchbot.me/script.js"></script>
+        <script>
+            window.sntchChat.Init(109433)
+        </script>
 </body>
 
 </html>
