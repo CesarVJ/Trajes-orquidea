f2b54cb45812e4b106ec2658052ad98856b8b06a
cesar valdez jacinto <cvj991@gmail.com>
2020-05-23 15:27:56 -0500

    Se agrego Paypal como metodo de pago

--- vista/InformacionProducto.php
+++ vista/InformacionProducto.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<script>
+var precio =0;
+var precioFinal = 0;
+var cantidadFinal=1;
+</script>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/estilos.css?v=<?php echo time(); ?>">
@@ -35,14 +40,17 @@
                 <p><b>Talla: </b><?php echo $producto->getTalla(); ?></p>
 
                 <label for="cantidad-articulos"><b>Cantidad:</b></label>
-                <input type="number" class="cantidad" name="cantidad-articulos" min="1" id="cantidad-articulos"> <br>
+                <input type="number" class="cantidad" name="cantidad-articulos" min="1" id="cantidad-articulos" required value="1"> <br>
                 <input type="hidden" id="v" value="0" />
-                <label><b>Precio:</b> <span id="precio-total"></span> $</label><br>
-                <input type="text" id="precioFinal" name="precioFinal" value="0" style="display:none;">
-                <input type="submit" class="btn btn-success btn-lg btn-block" value="Realizar compra" style="margin-top:5rem;">
+                <label><b>Precio:</b> <span id="precio-total"> <?php echo $producto->getPrecio();?></span> $</label><br>
+                <input type="text" id="precioFinal" name="precioFinal" value="<?php echo $producto->getPrecio(); ?>" style="display:none;">
+                <input type="submit" class="btn btn-success btn-lg btn-block" value="Realizar compra con tarjeta" style="margin-top:5rem; margin-bottom:1rem;">
+                <div id="paypal-button-container"></div>
+
                 <script>
+                var precioFinal = document.getElementById("precioFinal").value;
                     $("#cantidad-articulos").bind('keyup mouseup', function () {
-                        var precio = <?php echo $producto->getPrecio();?>;
+                        precio = <?php echo $producto->getPrecio();?>;
                         var actual = $("#cantidad-articulos").val();
                         var anterior = $("#v").val();
                         if (actual > anterior || actual < anterior) {
@@ -51,7 +59,34 @@
                             document.getElementById("precio-total").innerHTML = nuevo * precio;
                             document.getElementById("precioFinal").value = nuevo * precio;
                         }
+                        precioFinal = document.getElementById("precioFinal").value;
+                        cantidadFinal=$("#cantidad-articulos").val();
                     });
+                </script>
+                <script src="https://www.paypal.com/sdk/js?client-id=AcDGE4-QEPX4szmt34VvgNVBaepqx_nV0k3L4HAcYfbLO31P8kKN7mDB1biNtkHqi8hrGzhmMbocn-dz&currency=MXN"></script>
+                <!-- id-produccion: AcDGE4-QEPX4szmt34VvgNVBaepqx_nV0k3L4HAcYfbLO31P8kKN7mDB1biNtkHqi8hrGzhmMbocn-dz  -->
+                <!-- id-sandbox: AbhFTjthoDCInx6NEksWXsbU4F2OcSSwHXI9vmr6CDS4e5EslqHsPPhglwmxgZpAXRxtl5LlIUXkDG4q  -->
+                <script>
+                    paypal.Buttons({
+                        style: {
+                            layout: 'horizontal'
+                            },
+                            createOrder: function(data, actions) {
+                              return actions.order.create({
+                                purchase_units: [{
+                                amount: {
+                                    value: precioFinal
+                                }
+                            }]
+                        });
+                    },
+                    onApprove: function(data, actions) {
+                        return actions.order.capture().then(function(details) {
+                            location.href='../pagoExitoso.php?id=<?php echo $_GET['id'];?>&cantidad='+cantidadFinal+'&total='+precioFinal;
+                        alert('Transaccion completada exitosamente por ' + details.payer.name.given_name);
+                    });
+                    }
+                    }).render('#paypal-button-container');
                 </script>
             </div>
         </form>
